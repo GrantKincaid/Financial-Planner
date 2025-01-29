ee3e02c0bd0f3d1a34ca4eeefc661f3261111d00
GrantKincaid <grantkincaid@derivativezero.net>
2025-01-29 12:45:54 -0800

    more center bar changes

--- FinancialPlanner.py
+++ FinancialPlanner.py
@@ -333,7 +333,7 @@ class FinancialPlanner():
                 top: 0;
                 left: 50%;
                 transform: translateX(-50%);
-                width: clamp(50vw, 60vw, 1100px); /* Adapts width dynamically */
+                width: clamp(60vw, 70vw, 1100px); /* Adapts width dynamically */
                 min-width: 400px;
                 max-width: 1100px;
                 height: 100%;
